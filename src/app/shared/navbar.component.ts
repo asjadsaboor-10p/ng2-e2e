a3c41545534993239aae6373e0ef821931e51d68
asjadsaboor-10p <asjad.saboor@tenpearls.com>
2017-05-18 15:13:12 +0500

    add navigation bar compenent :zap:

---
+++ src/app/shared/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'navi-bar',
+    templateUrl: `
+        <div class="top-bar">
+            <div class="top-bar-title"> Gigabyte Gaming Company</div>
+            <div>
+                <ul class="menu">
+                    <li class="nav-menu"><a href="#">Menu Item 1</a></li>
+                </ul>
+            </div>
+        </div>
+    `,
+    styleUrls:['./navbar.component.css']
+})
+
+export class NavComponent {}
