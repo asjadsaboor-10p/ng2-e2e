4a6134d8d009da2945666eade2d9f131ca62890f
asjadsaboor-10p <asjad.saboor@tenpearls.com>
2017-05-18 16:00:11 +0500

    Add routing module :fireworks:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './start/app.component';
+
 import { NavComponent } from './shared/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
+import { AppRoutingModule} from './shared/app.routing';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
