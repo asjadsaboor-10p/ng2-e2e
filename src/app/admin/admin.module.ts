8735ac613bb4bf7d92a3a983fae3f07e53e7f602
asjadsaboor-10p <asjad.saboor@tenpearls.com>
2017-05-26 17:23:25 +0500

    Add Product add component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,9 +10,12 @@ import { SignUpComponent } from "./signUp/sign-up.component";
 
 import { UserService } from './adminShared/user.service';
 import { BlogAdminService } from "./adminShared/blog-admin.service";
+import { ProductAdminService } from "./adminShared/product-admin.service";
 
 import { BlogAdminComponent } from "./blogAdmin/blog-admin.component";
 import { BlogAddComponent } from "./blogAdd/blog-add.component";
+import { ProductAdminComponent } from "./productAdmin/product-admin.component";
+import { ProductAddComponent } from "./productAdd/product-add.component";
 
 import { TruncatePipe } from './adminShared/trunc.pipe'
 
@@ -22,6 +25,7 @@ const AdminRoutes: Routes = [
         component: AdminComponent,
         children: [
             { path: 'blog-admin', component: BlogAdminComponent, canActivate: [UserService] },
+            { path: 'product-admin', component: ProductAdminComponent, canActivate: [UserService] },
             { path: 'login', component: LoginComponent },
             { path: 'signup', component: SignUpComponent },
             { path: '', component: AdminMenuComponent, canActivate: [UserService] }
@@ -46,11 +50,14 @@ const AdminRoutes: Routes = [
         SignUpComponent,
         BlogAdminComponent,
         BlogAddComponent,
-        TruncatePipe
+        TruncatePipe,
+        ProductAdminComponent,
+        ProductAddComponent
     ],
     providers: [
         UserService,
-        BlogAdminService
+        BlogAdminService,
+        ProductAdminService
     ],
 })
 
